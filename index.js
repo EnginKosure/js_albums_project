99e32394c187b2e50f8919714a64fd9450707423
EnginKosure <engin.kosure@cronos.be>
2020-09-24 23:59:36 +0200

    apply sass, do header

--- index.js
+++ index.js
@@ -58,3 +58,12 @@ function checkPrevious(id){
         lastActiveBtn = 'btn1';
     }
 }
+
+let navbar = $(".navbar");
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 10) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
