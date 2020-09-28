5c8823d890789e687cc6c5af7f0cfe25ce8d2cbe
EnginKosure <engin.kosure@cronos.be>
2020-09-28 18:03:12 +0200

    refactor async fuctions

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const btn=document.getElementById('btn').addEventListener('click',getPhotos);
-const btn1=document.getElementById('btn1').addEventListener('click',getAlbums);
-const prev=document.getElementById('prev').addEventListener('click',getPhotos);
-const middle=document.getElementById('middle').addEventListener('click',getPhotos);
-const next=document.getElementById('next').addEventListener('click',getPhotos);
+// const btn=document.getElementById('btn').addEventListener('click',getPhotos);
+// const btn1=document.getElementById('btn1').addEventListener('click',getAlbums);
+document.getElementById('prev').addEventListener('click',getPhotos);
+const middle=document.getElementById('middle');
+middle.addEventListener('click', getPhotos)
+document.getElementById('next').addEventListener('click',getPhotos);
 const prev1=document.getElementById('prev1').addEventListener('click',getAlbums);
-const middle1=document.getElementById('middle1').addEventListener('click',getAlbums);
+const middle1=document.getElementById('middle1');
+middle1.addEventListener('click',getAlbums);
 const next1=document.getElementById('next1').addEventListener('click',getAlbums);
+let pgnCount=1;
+let pgnCount1=1;
 
 
 let lastActiveBtn;
@@ -15,54 +19,73 @@ let clickCountDiv1=0;
 const div=document.getElementById('cardDiv');
 const div1=document.getElementById('cardDiv1');
 
-
-function getPhotos(e){
-    resetCounters(e.target.id)
-
-    fetch('https://jsonplaceholder.typicode.com/photos')
-    .then(res=>res.json())
-    .then(post=>{
-        for(let i=0;i<9;i++){
+//Get photos data from API
+async function getPhotoData(){
+    const photoStream=await fetch(`https://jsonplaceholder.typicode.com/photos/?_page=${pgnCount}`);
+    const photos=await photoStream.json();
+    photos.forEach(photo=>{
+        console.log(photo);
             div.innerHTML+=`
-                <div class='card col-lg-3 col-md-4 col-sm-6 mx-1 d-flex shadow card-img-top m-2'>
-                    <img class='rounded-circle w-150 p-2' src='${post[count].thumbnailUrl}'>
-                    <div class="card-body text-center">
-                        <h5 class="card-title title text-muted">${post[count].id}</h5>
-                        <p class="card-text subtext">${post[count].title}</p>
-                    </div>
+            <div class='card col-lg-3 col-md-4 col-sm-6 mx-1 d-flex shadow card-img-top m-2'>
+                <img class='rounded-circle w-150 p-2' src='${photo.thumbnailUrl}'>
+                <div class="card-body text-center">
+                    <h5 class="card-title title text-muted">${photo.id}</h5>
+                    <p class="card-text subtext">${photo.title}</p>
                 </div>
-            `
-            count+=1
-        }
+            </div>
+        `
     })
-    .catch(error=>console.log(error))
 }
-
-function getAlbums(e){
+function getPhotos(e){
+    e.preventDefault();
     resetCounters(e.target.id)
-
-    fetch('https://jsonplaceholder.typicode.com/albums')
-    .then(res=>res.json())
-    .then(post=>{
-        for(let i=0;i<9;i++){
+    if (e.target.id=='prev' && pgnCount>1){
+        pgnCount-=1  
+    }else if (e.target.id=='next'){
+        pgnCount+=1
+    }else{
+        pgnCount=1 
+    }
+    middle.innerText=`Reset | ${pgnCount*10-9} - ${pgnCount*10}`;
+    getPhotoData();
+}
+//Get albums data
+async function getAlbumData(){
+    const albumStream=await fetch(`https://jsonplaceholder.typicode.com/albums?_page=${pgnCount1}`);
+    const albums=await albumStream.json();
+    albums.forEach(album=>{
+        console.log('2',album.id);
             div1.innerHTML+=`
-                <div class='card col-lg-3 col-md-4 col-sm-6 mx-1 d-flex shadow card-img-top m-2'>
-                    <div class="card-body text-center">
-                        <a href="#btn" class="returnValue">
-                            <h5 class="card-title title text-muted">${post[count].id}</h5>
-                        </a>
-                        <p class="card-text subtext">${post[count].title}</p>
-                    </div>
+            <div class='card col-lg-3 col-md-4 col-sm-6 mx-1 d-flex shadow card-img-top m-2'>
+                <div class="card-body text-center">
+                    <a href="#btn" class="returnValue">
+                        <h5 class="card-title title text-muted">${album.id}</h5>
+                    </a>
+                    <p class="card-text subtext">${album.title}</p>
                 </div>
-            `
-            count+=1
-        }
-    })
-    .catch(error=>console.log(error))
-    .finally(()=>document.querySelectorAll('.returnValue').forEach(el=>el.addEventListener('click',getNthAlbPhotos)))
+            </div>
+        `
+    });
+    (function () {
+        return document.querySelectorAll('.returnValue').forEach(el => el.addEventListener('click', getNthAlbPhotos));
+    })();
+}
+function getAlbums(e){
+    e.preventDefault();
+    resetCounters(e.target.id)
+    if (e.target.id=='prev1' && pgnCount1>1){
+        pgnCount1-=1  
+    }else if (e.target.id=='next1'){
+        pgnCount1+=1
+    }else{
+        pgnCount1=1 
+    }
+    middle1.innerText=`Reset | ${pgnCount1*10-9} - ${pgnCount1*10}`;
+    getAlbumData();
 }
 
 function getNthAlbPhotos(e){
+    e.preventDefault()
     resetCounters(e.target)
 
     fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${e.target.innerText}`)
@@ -84,53 +107,20 @@ function getNthAlbPhotos(e){
     .catch(error=>console.log(error))
 }
 
-
-// function checkPrevious(id){
-//     if(div.innerHTML!=='' && lastActiveBtn!==id){
-//         div.style.minHeight='1200px';
-//         div.innerHTML='';
-//         count=0;
-//     }
-//     id=='btn' ? lastActiveBtn = id : lastActiveBtn = 'btn1'
-// }
-
 function resetCounters(id){
+    const checkArr=['next','prev','middle']
+    const checkArr1=['next1','prev1','middle1']
     div1.innerHTML='';
     div.innerHTML='';
 
-    if( id=='btn'){
-        clickCountDiv+=1;
-        if(clickCountDiv<=1){
-            div.style.minHeight='1200px';
-            div1.style.minHeight='0';
-            clickCountDiv1=0;
-            count=0;
-        }
-    }else if (id=='btn1'){
-        clickCountDiv1+=1;
-        if(clickCountDiv1<=1){
-            div1.style.minHeight='1200px';
-            div.style.minHeight='0';
-            clickCountDiv=0;
-            count=0;
-        }
-    }else{
+    if( checkArr.includes(id)){
         div.style.minHeight='1200px';
         div1.style.minHeight='0';
-        clickCountDiv1=0;
-        clickCountDiv=0;
-        count=0;
+    }else if (checkArr1.includes(id)){
+        div1.style.minHeight='1200px';
+        div.style.minHeight='0';
     }
 }
-
-// let navbar = $(".navbar");
-// $(window).scroll(function () {
-//   if ($(window).scrollTop() > 10) {
-//     navbar.addClass("sticky");
-//   } else {
-//     navbar.removeClass("sticky");
-//   }
-// });
 
 // When the user scrolls the page, execute makeSticky
 window.onscroll = function() {makeSticky()};
